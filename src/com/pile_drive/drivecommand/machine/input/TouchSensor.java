e401cf6182f737001339ec929c8f902ef60c3c54
tiwanari <tatsuyaw0c@gmail.com>
2014-06-19 21:38:31 +0900

    added TouchSensorTest and TouchSensor

---
+++ src/com/pile_drive/drivecommand/machine/input/TouchSensor.java
@@ -0,0 +1,44 @@
+package com.pile_drive.drivecommand.machine.input;
+
+import java.util.HashMap;
+
+import com.pile_drive.drivecommand.command.CommandBase;
+import com.pile_drive.drivecommand.command.CommandFactory;
+import com.pile_drive.drivecommand.machine.DeviceBase;
+import com.pile_drive.drivecommand.machine.DeviceType;
+import com.pile_drive.drivecommand.model.CommandType;
+import com.pile_drive.drivecommand.model.ProtocolBase;
+
+public class TouchSensor extends DeviceBase {
+	
+	public TouchSensor(int port, ProtocolBase protocol) {
+		super(port, protocol);
+	}
+	
+	/**
+	 * Check this sensor is touched now
+	 * 
+	 * @return boolean
+	 */
+	public boolean isTouched() {
+		CommandBase cmd = CommandFactory.createCommand(CommandType.GET_TOUCH_TOUCHED, null);
+		HashMap<String, Object> res = exec(cmd);
+		return (Boolean) res.get("value");
+	}
+	
+	/**
+	 * Get the touched count
+	 * 
+	 * @return number of count
+	 */
+	public int getTouchedCount() {
+		CommandBase cmd = CommandFactory.createCommand(CommandType.GET_TOUCH_COUNT, null);
+		HashMap<String, Object> res = exec(cmd);
+		return (Integer) res.get("value");
+	}
+	
+	@Override
+	public DeviceType getDeviceType() {
+		return DeviceType.TOUCH_SENSOR;
+	}
+}
