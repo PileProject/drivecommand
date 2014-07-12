4102e70bc99719b7bd2f29cc6b639fdb859d21f6
a-kawakami <akane.k.0820@gmail.com>
2014-07-12 20:39:38 +0900

    implemented motor forward/backward/stop and tested them.

--- src/com/pile_drive/drivecommand/model/nxt/NxtProtocol.java
+++ src/com/pile_drive/drivecommand/model/nxt/NxtProtocol.java
@@ -1,5 +1,7 @@
 package com.pile_drive.drivecommand.model.nxt;
 
+import static com.pile_drive.drivecommand.model.nxt.NxtConstants.*;
+
 import java.io.IOException;
 import java.util.HashMap;
 
@@ -73,7 +75,7 @@ public class NxtProtocol extends ProtocolBase {
 			}
 			case SET_BUZZER_OFF: {
 				break;
-				}
+			}
 			case SET_BUZZER_ON: {
 				break;
 			}
@@ -84,6 +86,10 @@ public class NxtProtocol extends ProtocolBase {
 				break;
 			}
 			case SET_MOTOR_SPEED: {
+				HashMap<String, Object> args = cmd.getArgs();
+				int speed = (Integer) args.get("speed");
+				setOutputState(port, speed, BRAKE + MOTORON + REGULATED,
+						REGULATION_MODE_MOTOR_SPEED, 0, MOTOR_RUN_STATE_RUNNING, 0);
 				break;
 			}
 			case SET_SERVO_ANGLE: {
@@ -94,5 +100,61 @@ public class NxtProtocol extends ProtocolBase {
 			}
 		}
 		return res;
+	}
+	
+	/**
+	 * Set output device condition.
+	 * 
+	 * @param port
+	 *            The port of the device (0 ~ 3).
+	 * @param speed
+	 *            The speed of the device.
+	 * @param mode
+	 *            The mode of the device. (ex. MOTORON, BRAKE, and/or
+	 *            REGULATED).This field is a bitfield.
+	 * @param regulationMode
+	 *            see NxtConstants.
+	 * @param turnRatio
+	 *            Use this parameter to move more than two motor.
+	 * @param runState
+	 *            see NxtConstants.
+	 * @param tachoLimit
+	 *            - Number of degrees to rotate before stopping.
+	 */
+	private void setOutputState(int port, int speed, int mode,
+			int regulationMode, int turnRatio, int runState, int tachoLimit) {
+		byte[] request = {
+			DIRECT_COMMAND_NOREPLY,
+			SET_OUTPUT_STATE,
+			(byte) port,
+			(byte) speed,
+			(byte) mode,
+			(byte) regulationMode,
+			(byte) turnRatio,
+			(byte) runState,
+			(byte) tachoLimit,
+			(byte) (tachoLimit >>> 8),
+			(byte) (tachoLimit >>> 16),
+			(byte) (tachoLimit >>> 24)
+		};
+		// Send request
+		sendData(request);
+	}
+	
+	/**
+	 * A small helper to send data. This method calculates the size of the data
+	 * and append it to the data.
+	 * 
+	 * @param request
+	 */
+	private void sendData(byte[] request) {
+		// Calculate the size of request and append them.
+		byte[] data = new byte[request.length + 2];
+		data[0] = (byte) request.length;
+		data[1] = (byte) (request.length >> 8);
+		System.arraycopy(request, 0, data, 2, request.length);
+		
+		// Send request
+		mCommunicator.write(data, TIMEOUT);
 	}
 }
