4ec561c2fe693e5c890e5749f63147bb659ae3c3
a-kawakami <akane.k.0820@gmail.com>
2014-07-21 18:08:30 +0900

    solved the "not initialized at first time" problem.

--- src/com/pile_drive/drivecommand/model/nxt/NxtProtocol.java
+++ src/com/pile_drive/drivecommand/model/nxt/NxtProtocol.java
@@ -4,6 +4,9 @@ import static com.pile_drive.drivecommand.model.nxt.NxtConstants.*;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Map;
+
+import android.util.Log;
 
 import com.pile_drive.drivecommand.command.CommandBase;
 import com.pile_drive.drivecommand.model.CommandType;
@@ -11,13 +14,16 @@ import com.pile_drive.drivecommand.model.ProtocolBase;
 import com.pile_drive.drivecommand.model.com.ICommunicator;
 
 /**
- * @see <a href="http://sourceforge.net/projects/lejos/files/lejos-NXJ/">LeJOS</a>
+ * @see <a
+ *      href="http://sourceforge.net/projects/lejos/files/lejos-NXJ/">LeJOS</a>
  */
 public class NxtProtocol extends ProtocolBase {
 	private static final String KEY_VALUE = "value";
 	private static final int TIMEOUT = 1000;
 	private static final String TAG = "NxtProtocol";
-
+	private static final int MAX_RES_LENGTH = 66;
+	private Map<Integer, Byte> mPortTypes;
+	
 	public NxtProtocol(ICommunicator comm) {
 		super(comm);
 	}
@@ -25,11 +31,13 @@ public class NxtProtocol extends ProtocolBase {
 	@Override
 	public void open() throws IOException {
 		mCommunicator.open();
+		mPortTypes = new HashMap<Integer, Byte>();
 	}
 	
 	@Override
 	public void close() {
 		mCommunicator.close();
+		mPortTypes = null;
 	}
 	
 	@Override
@@ -52,7 +60,7 @@ public class NxtProtocol extends ProtocolBase {
 			case GET_LINE_VALUE: {
 				setInputMode(port, LIGHT_ACTIVE, PCTFULLSCALEMODE);
 				InputValues values = getInputValues(port);
-				res.put(KEY_VALUE, (int)(values.scaledValue / 10));
+				res.put(KEY_VALUE, (int) (values.scaledValue / 10));
 				break;
 			}
 			case GET_RANGEFINDER_DIST: {
@@ -70,7 +78,7 @@ public class NxtProtocol extends ProtocolBase {
 			case GET_SOUND_DB: {
 				setInputMode(port, SOUND_DB, PCTFULLSCALEMODE);
 				InputValues values = getInputValues(port);
-				res.put(KEY_VALUE, (int)(values.scaledValue / 10));
+				res.put(KEY_VALUE, (int) (values.scaledValue / 10));
 				break;
 			}
 			case GET_TOUCH_COUNT: {
@@ -123,7 +131,7 @@ public class NxtProtocol extends ProtocolBase {
 	 *            The speed of the device.
 	 * @param mode
 	 *            The mode of the device. (ex. MOTORON, BRAKE, and/or
-	 *            REGULATED).This field is a bitfield.
+	 *            REGULATED). This field is a bitfield.
 	 * @param regulationMode
 	 *            see NxtConstants.
 	 * @param turnRatio
@@ -168,18 +176,21 @@ public class NxtProtocol extends ProtocolBase {
 		
 		// Send request
 		mCommunicator.write(data, TIMEOUT);
-	}	
-
+	}
+	
 	private InputValues getInputValues(int port) {
-		// !! Needs to check port to verify they are correct ranges.
-		byte[] request = { DIRECT_COMMAND_REPLY, GET_INPUT_VALUES, (byte) port };
+		byte[] request = {
+			DIRECT_COMMAND_REPLY,
+			GET_INPUT_VALUES,
+			(byte) port
+		};
 		sendData(request);
-		byte[] reply = mCommunicator.read(66, TIMEOUT);
+		byte[] reply = mCommunicator.read(MAX_RES_LENGTH, TIMEOUT);
 		InputValues inputValues = new InputValues();
 		inputValues.inputPort = reply[3];
 		// 0 is false, 1 is true.
 		inputValues.valid = (reply[4] != 0);
-		// 0 is false, 1 is true. 
+		// 0 is false, 1 is true.
 		inputValues.isCalibrated = (reply[5] == 0);
 		inputValues.sensorType = reply[6];
 		inputValues.sensorMode = reply[7];
@@ -187,25 +198,55 @@ public class NxtProtocol extends ProtocolBase {
 		inputValues.normalizedADValue = (short) ((0xFF & reply[10]) | ((0xFF & reply[11]) << 8));
 		inputValues.scaledValue = (short) ((0xFF & reply[12]) | ((0xFF & reply[13]) << 8));
 		inputValues.calibratedValue = (short) ((0xFF & reply[14]) | ((0xFF & reply[15]) << 8));
-
+		
 		return inputValues;
 	}
 	
 	/**
 	 * Tells the NXT what type of sensor you are using and the mode to operate
 	 * in.
-	 *
+	 * 
 	 * @param port
-	 *            - 0 to 3
+	 *            The port of the device (0 ~ 3).
 	 * @param sensorType
-	 *            - Enumeration for sensor type (see NXTProtocol)
+	 *            see NxtConstants
 	 * @param sensorMode
-	 *            - Enumeration for sensor mode (see NXTProtocol)
+	 *            see NxtConstants
 	 */
 	public void setInputMode(int port, int sensorType, int sensorMode) {
-		// !! Needs to check port to verify they are correct ranges.
-		byte[] request = { DIRECT_COMMAND_NOREPLY, SET_INPUT_MODE, (byte) port,
-				(byte) sensorType, (byte) sensorMode };
-		sendData(request);
+		// If the port is not initialized, set the mode
+		if (!mPortTypes.containsKey(port) || sensorType != mPortTypes.get(port)) {
+			// Save the setting to a map and set the mode of sensor
+			mPortTypes.put(port, (byte) sensorType);
+			Log.d(TAG, "port(" + port + ") is initailized to " + sensorType);
+			
+			byte[] request = {
+				DIRECT_COMMAND_NOREPLY,
+				SET_INPUT_MODE,
+				(byte) port,
+				(byte) sensorType,
+				(byte) sensorMode
+			};
+			
+			sendData(request);
+			getInputValues(port); // Skip the first value (it may be invalid)
+			
+			// Sound sensor needs more initializing time based on our experiments.
+			if (sensorType == SOUND_DB) waitMillSeconds(250);
+		}
+	}
+	
+	/**
+	 * A helper method to wait for specified mill seconds.
+	 * 
+	 * @param millseconds
+	 */
+	private void waitMillSeconds(int millseconds) {
+		try {
+			Thread.sleep(millseconds);
+		}
+		catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 }
