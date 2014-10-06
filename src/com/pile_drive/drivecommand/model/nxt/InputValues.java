3e07954e5c8346618f146574d8264315c7419111
a-kawakami <akane.k.0820@gmail.com>
2014-10-06 22:43:30 +0900

    reflected shimasan's comments

--- src/com/pile_drive/drivecommand/model/nxt/InputValues.java
+++ src/com/pile_drive/drivecommand/model/nxt/InputValues.java
@@ -21,15 +21,13 @@ public class InputValues {
 	public int rawADValue;
 	/**
 	 * The normalized value from the Analog to Digital (AD) converter. I really
-	 * don't
-	 * know for sure which values are normalized yet.
+	 * don't know for sure which values are normalized yet.
 	 * 0 to 1023
 	 */
 	public int normalizedADValue;
 	/**
 	 * The scaled value starts working after the first call to the sensor.
-	 * The first value will be the raw value, but after that it produces scaled
-	 * values.
+	 * The first value will be the raw value, but after that it produces scaled values.
 	 * With the touch sensor, off scales to 0 and on scales to 1.
 	 */
 	public short scaledValue;
