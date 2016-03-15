4e3efa0a2fb09609e38d215fb09d72f33a7e8f53
Yusaku Mandai <mandai.yusaku@gmail.com>
2016-03-15 23:38:41 +0900

    Remove timeout from ICommunicator#write and ICommunicator#read

--- src/main/java/com/pileproject/drivecommand/model/com/ICommunicator.java
+++ src/main/java/com/pileproject/drivecommand/model/com/ICommunicator.java
@@ -36,18 +36,16 @@ public interface ICommunicator {
      * Write the byte array data to the device.
      *
      * @param data
-     * @param timeout Max waiting time for the end of this communication. [Unit: ms]
      * @throws RuntimeException
      */
-    void write(byte[] data, int timeout) throws RuntimeException;
+    void write(byte[] data) throws RuntimeException;
 
     /**
      * Read the response from the device.
      *
      * @param length  The max length of response wanted to be read.
-     * @param timeout Max waiting time for the end of this communication. [Unit: ms]
      * @return
      * @throws RuntimeException
      */
-    byte[] read(int length, int timeout) throws RuntimeException;
+    byte[] read(int length) throws RuntimeException;
 }
