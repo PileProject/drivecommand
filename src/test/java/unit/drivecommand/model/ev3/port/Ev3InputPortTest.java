ec1679575a7c63c5946607b79c6f6e982c572c3e
tiwanari <tatsuyaw0c@gmail.com>
2016-12-21 00:29:28 +0900

    use a plugin to maintain the license automatically

--- src/test/java/unit/drivecommand/model/ev3/port/Ev3InputPortTest.java
+++ src/test/java/unit/drivecommand/model/ev3/port/Ev3InputPortTest.java
@@ -1,5 +1,5 @@
-/*
- * Copyright (C) 2011-2015 PILE Project, Inc. <dev@pileproject.com>
+/**
+ * Copyright (C) 2011-2016 The DriveCommand Authors <pile-dev@googlegroups.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,9 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  */
-
 package unit.drivecommand.model.ev3.port;
 
 import com.pileproject.drivecommand.model.ev3.port.Ev3InputPort;
