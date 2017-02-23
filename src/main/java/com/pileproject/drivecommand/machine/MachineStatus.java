fa4377673e5b9194c6f33983942ac7d71a658c85
tiwanari <tatsuyaw0c@gmail.com>
2017-02-23 21:49:54 +0900

    update the author notice of LICENSE

--- src/main/java/com/pileproject/drivecommand/machine/MachineStatus.java
+++ src/main/java/com/pileproject/drivecommand/machine/MachineStatus.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2011-2016 The DriveCommand Authors <pile-dev@googlegroups.com>
+ * Copyright (C) 2011-2017 The PILE Developers <pile-dev@googlegroups.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
