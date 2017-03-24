e208d2587a546c5983c67a5b2f8a613232c67267
tiwanari <tatsuyaw0c@gmail.com>
2017-03-25 00:05:29 +0900

    Apply myusak's comments

--- scripts/deploy-gh-pages.sh
+++ scripts/deploy-gh-pages.sh
@@ -1,53 +1,25 @@
 #!/bin/bash
-set -e # Exit with nonzero exit code if anything fails
+set -eu
 
-mkdir tmp
-cd tmp
-
-SOURCE_BRANCH="master"
-TARGET_BRANCH="gh-pages"
-
-# Pull requests and commits to other branches shouldn't try to deploy, just build to verify
-if [ "$TRAVIS_PULL_REQUEST" != "false" -o -z "$TRAVIS_TAG" ]; then
-    echo "Skipping deploy"
-    exit 0
-fi
-
-# Save some useful information
+# get the remote URL
 REPO=`git config remote.origin.url`
 REPO_WITH_TOKEN=${REPO/https:\/\/github.com\//https://${GH_TOKEN}@github.com/}
 
-# Clone the existing gh-pages for this repo into out/
-# Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
-git clone $REPO src
-cd src; git checkout $SOURCE_BRANCH
+rootdir=`pwd`
+tmpdir="${rootdir}/`mktemp -d`"
 
-# Build
-./gradlew uploadArchives
+git clone $REPO $tmpdir
+cd $tmpdir
 
-cd ..
+# build a release
+./gradlew deploy -Ptag=$TRAVIS_TAG
 
-git clone $REPO tgt
-cd tgt; git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
-
-cd ..
-
-# Copy output files
-cp -r src/com/pileproject/drivecommand/* tgt/com/pileproject/drivecommand
-
-# Now let's go have some fun with the cloned repo
-cd tgt
+# commit the changes
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis"
 
-# If there are no changes (e.g. this is a README update) then just bail.
-if [ -z `git diff --exit-code` ]; then
-    echo "No changes to the spec on this push; exiting."
-    exit 0
-fi
-
-# Commit the "changes", i.e. the new version.
-# The delta will show diffs between new and old versions.
 git add .
 git commit -m "Release ${TRAVIS_TAG}"
-git push --quiet $REPO_WITH_TOKEN $TARGET_BRANCH
+git push --quiet $REPO_WITH_TOKEN master
+
+echo "Released ${TRAVIS_TAG}"
