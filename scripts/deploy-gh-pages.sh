4af788ae38e7317ff0b7f8bce0d65275114f77c1
Tatsuya Iwanari <tiwanari@users.noreply.github.com>
2017-03-26 06:40:59 +0900

    Introduce an auto deploy feature (#22)
    resolves #20.
    
    This PR introduces a feature of auto deploy.
    
    Procedure
    1. Merge changes into `master` (e.g., `git checkout master; git merge develop`)
    1. Make a tag (e.g., `git tag v2.2.0`)
    1. Push changes (`git push --follow-tags`)
    
    Note
    I specify the format of a tag for deploy to `^v[0-9].*$` and added a feature removing the prefix `v` in the deploy task to follow the current naming rule of release folders.

---
+++ scripts/deploy-gh-pages.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -eu
+
+# get the remote URL
+REPO=`git config remote.origin.url`
+REPO_WITH_TOKEN=${REPO/https:\/\/github.com\//https://${GH_TOKEN}@github.com/}
+
+tmpdir=`mktemp -d`
+
+git clone -b master $REPO $tmpdir
+cd $tmpdir
+
+# build a release
+./gradlew deploy -Ptag=$TRAVIS_TAG
+
+# commit the changes
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis"
+
+git add .
+git commit -m "Release ${TRAVIS_TAG}"
+git push --quiet $REPO_WITH_TOKEN master
+
+echo "Released ${TRAVIS_TAG}"
